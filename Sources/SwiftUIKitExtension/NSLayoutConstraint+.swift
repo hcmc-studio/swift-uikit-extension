91cb83dd0f1c7dadf4bf702c20b34a0838b6f7a6
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-15 21:32:07 +0900

    [Script push]

--- Sources/SwiftUIKitExtension/NSLayoutConstraint+.swift
+++ Sources/SwiftUIKitExtension/NSLayoutConstraint+.swift
@@ -27,6 +27,30 @@ extension UIView {
     }
     
     public func fit(
+        equalTo dst: UIView,
+        top: CGFloat? = .zero,
+        leading: CGFloat? = .zero,
+        trailing: CGFloat? = .zero,
+        bottom: CGFloat? = .zero
+    ) -> [NSLayoutConstraint] {
+        var constraints = [NSLayoutConstraint]()
+        if let top = top {
+            constraints.append(topAnchor.constraint(equalTo: dst.topAnchor, constant: top))
+        }
+        if let leading = leading {
+            constraints.append(leadingAnchor.constraint(equalTo: dst.leadingAnchor, constant: leading))
+        }
+        if let trailing = trailing {
+            constraints.append(trailingAnchor.constraint(equalTo: dst.trailingAnchor, constant: trailing))
+        }
+        if let bottom = bottom {
+            constraints.append(bottomAnchor.constraint(equalTo: dst.bottomAnchor, constant: bottom))
+        }
+        
+        return constraints
+    }
+    
+    public func fit(
         equalTo dst: UILayoutGuide,
         top: CGFloat? = .zero,
         leading: CGFloat? = .zero,
