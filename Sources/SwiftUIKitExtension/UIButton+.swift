3aaaedfb77b7af35bbac9540b34effa95b00f556
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-17 12:40:13 +0900

    [Script push]

--- Sources/SwiftUIKitExtension/UIButton+.swift
+++ Sources/SwiftUIKitExtension/UIButton+.swift
@@ -28,3 +28,19 @@ extension UIViewExtension where Self: UIButton {
         addTarget(self, action: #selector(invokeOnClick), for: .touchUpInside)
     }
 }
+
+@available(iOS 15.0, *)
+extension UIButton.Configuration {
+    public static func icon(_ icon: UIImage) -> UIButton.Configuration {
+        var configuration = UIButton.Configuration.borderless()
+        configuration.imagePadding = 0
+        configuration.contentInsets = .zero
+        configuration.image = icon.withRenderingMode(.alwaysTemplate)
+        
+        return configuration
+    }
+    
+    public static func icon(system name: String) -> UIButton.Configuration {
+        icon(.init(systemName: name)!)
+    }
+}
