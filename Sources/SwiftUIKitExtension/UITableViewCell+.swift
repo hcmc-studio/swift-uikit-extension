bf97637ad1b793d351e972ccb2dd159015a05fa4
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-17 12:48:06 +0900

    [Script push]

--- Sources/SwiftUIKitExtension/UITableViewCell+.swift
+++ Sources/SwiftUIKitExtension/UITableViewCell+.swift
@@ -10,17 +10,26 @@ import UIKit
 
 open class XUITableViewCell: UITableViewCell, UIViewExtension {
     public var onClick: (() -> Void)? = nil
+    public let contentContainer = XUIView()
     private var task: Task<Void, any Error>? = nil
     private var isViewPresent = false
     
     public override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         addOnClickRecognizer()
+        prepareContainer()
     }
     
     public required init?(coder: NSCoder) {
         super.init(coder: coder)
         addOnClickRecognizer()
+        prepareContainer()
+    }
+    
+    private func prepareContainer() {
+        contentView.addSubview(contentContainer)
+        contentContainer.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate(contentContainer.fit(equalTo: contentView))
     }
     
     func bind() {
@@ -34,10 +43,12 @@ open class XUITableViewCell: UITableViewCell, UIViewExtension {
         )
     }
     
+    /// View를 초기화하는 블록입니다. 1회만 실행됩니다.
     open func prepareView() {}
     
     open func fetch() async throws {}
     
+    /// View의 내용을 초기화하는 블록입니다. 매 bind마다 실행됩니다.
     open func bindView(error: Error?) {}
     
     open override func prepareForReuse() {
