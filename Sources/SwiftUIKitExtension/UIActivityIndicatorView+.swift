74329af0d0b07894d34a1afc292659227c3ccd0b
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-18 16:33:07 +0900

    [Script push]

---
+++ Sources/SwiftUIKitExtension/UIActivityIndicatorView+.swift
@@ -0,0 +1,11 @@
+//
+//  UIActivityIndicatorView+.swift
+//
+//
+//  Created by Ji-Hwan Kim on 10/18/23.
+//
+
+import Foundation
+import UIKit
+
+open class XUIActivityIndicatorView: UIActivityIndicatorView {}
