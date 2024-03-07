acb723980c95d63b86151c19f2804f44713fee3d
Ji-Hwan KIM <nodealdev@gmail.com>
2024-03-07 16:15:34 +0900

    [Script push]

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/hcmc-studio/swift-concurrency-extension.git",
-            branch: "0.0.70"
+            branch: "0.0.71"
         ),
         .package(
             url: "https://github.com/hcmc-studio/swift-quartz-core-extension.git",
-            branch: "0.0.70"
+            branch: "0.0.71"
         ),
     ],
     targets: [
